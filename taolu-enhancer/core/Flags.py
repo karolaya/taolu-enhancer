17e8d857fcf00e02a77e77b6d72d706cf68f6bde
Karolay Ardila Salazar <karolayardila@hotmail.com>
2017-04-04 22:06:04 -0500

    Regeneration

--- taolu-enhancer/core/Flags.py
+++
@@ -1,9 +0,0 @@
-class Flags:
-    CONFIGURATION_FLAGS = {
-            'DEFAULT_FORM': ('KATA', 'TAOLU',),
-            'KINECT_COUNT': (1,),
-            'ERROR_FLAGS': ('ACUM', 'PERCENT',),
-            'LISTENER_COUNT': (1,),
-            'TRAINING_DIRS': ('$HOME/.taolu/trainer/',),
-            'CONFIG_DIRS': ('$HOME/.taolu/config/',),
-        }
