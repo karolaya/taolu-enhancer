6dbab14bbc33b0fd0cde08d9da526f214900b1d4
asmateus <mateus@uninorte.edu.co>
2017-05-06 14:47:14 -0500

    Angle calcualtion done based on inner product

---
+++ taolu-enhancer/utils/angle_calculator.py
@@ -0,0 +1,21 @@
+import numpy as np
+from collections import namedtuple
+from definitions import Joints
+from definitions import Projections
+
+Joint = namedtuple('Joint', ['x', 'y', 'z'])
+
+def calculateAngleProjection(central, superior, inferior):
+    # Build vectors based on request
+    sup_vector = np.array([superior.x - central.x, superior.y - central.y, superior.z - central.z])
+    inf_vector = np.array([inferior.x - central.x, inferior.y - central.y, inferior.z - central.z])
+
+    angle = np.dot(sup_vector, inf_vector)/(np.linalg.norm(sup_vector)*np.linalg.norm(inf_vector))
+
+    return angle
+
+a = Joint(2, 2, 2)
+b = Joint(3, 3.5, 3)
+c = Joint(4, 4, 4.2)
+
+print(calculateAngleProjection(a, b, c))
