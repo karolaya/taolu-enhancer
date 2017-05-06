23170821c6e4580f0cd1bc2e0d68ace604cb5156
asmateus <mateus@uninorte.edu.co>
2017-05-06 15:15:11 -0500

    Fixed angle error for 0 approximations

--- taolu-enhancer/utils/angle_calculator.py
+++ taolu-enhancer/utils/angle_calculator.py
@@ -2,6 +2,8 @@ import numpy as np
 from collections import namedtuple
 from definitions import Joints
 
+OFFSET = 0.1
+
 Joint = namedtuple('Joint', ['x', 'y', 'z'])
 
 def calculateAngleProjection(central, superior, inferior):
@@ -9,6 +11,8 @@ def calculateAngleProjection(central, superior, inferior):
     sup_vector = np.array([superior.x - central.x, superior.y - central.y, superior.z - central.z])
     inf_vector = np.array([inferior.x - central.x, inferior.y - central.y, inferior.z - central.z])
 
-    angle = np.dot(sup_vector, inf_vector)/(np.linalg.norm(sup_vector)*np.linalg.norm(inf_vector))
+    cos_angle = np.dot(sup_vector, inf_vector)/(np.linalg.norm(sup_vector)*np.linalg.norm(inf_vector))
+    cos_angle = float('%.5f'%(cos_angle))
+    angle = np.arccos(cos_angle)
 
     return angle
