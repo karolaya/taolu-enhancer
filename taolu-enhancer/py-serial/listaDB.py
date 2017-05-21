91dae73e00f770600ea053db730f7d073d86c6e8
Karolay Ardila Salazar <karolayardila@hotmail.com>
2017-05-21 15:42:59 -0500

    extraction joints c++

--- taolu-enhancer/py-serial/listaDB.py
+++ taolu-enhancer/py-serial/listaDB.py
@@ -1,15 +1,21 @@
 #cmm = ['x,y,z;x1,y1,z1;x2,y2,z2;','x,y,z;x1,y1,z1;x2,y2,z2;','x,y,z;x1,y1,z1;x2,y2,z2;']
-
+import numpy as np
 def listaDB(cmm):
-    lst = ['form']
+    lst = []
+    vals = []
+    lst_db = []
 
     for k in range(len(cmm)):
-        cmm_s = cmm[k].split(";")
-        del cmm_s[-1]
-        i = 0
-        for i in range(len(cmm_s)):
-            lst.append(cmm_s[i])
-        lst.append("form")
-    del lst[-1]
-    for j in range(0, len(lst), lst[1:].index('form') + 1):
-        print(lst[j:j + lst[1:].index('form') + 1])
+        cmm_s = ','.join(cmm[k].split(';'))[0:-1].split(',')[0:-1]
+        if cmm_s:
+            #print(len(cmm_s))
+            lst.append(cmm_s)
+
+    for j in range(60):
+        vals.append(np.mean([float(a[j]) for a in lst]))
+    #print(vals)
+
+    for i in range(0, len(vals), 3):
+        s_db = str(vals[i]) + ',' + str(vals[i + 1]) + ',' + str(vals[i + 2])
+        lst_db.append(s_db)
+    #print(lst_db)
